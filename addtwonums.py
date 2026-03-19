b00bf3f7d40646589a08add4aa48aae1aee87ac4
Hu Hantao <2486419575@qq.com>
2026-03-19 15:28:06 +0800

    Adding addtwonums.py to reposiyory

--- addtwonums.py
+++ addtwonums.py
@@ -1,5 +1,13 @@
+#Add two numbers in python
+#Author:Hu Hantao
+def add(a,b):
+	result=float(a)+float(b)
+	return result
+
+
 a=input("First number:")
 b=input("Second number:")
-res=float(a)+float(b)
+res=add(a,b)
+print("The Answer is:")
 print(res)
 
